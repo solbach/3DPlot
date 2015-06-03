978a6b592591471f2d9c07a9c2b348dc11539b1d
Markus Solbach <mksolb@gmail.com>
2015-06-03 16:12:30 -0400

    Update Plot3D.m

--- Plot3D.m
+++ Plot3D.m
@@ -2,13 +2,11 @@
 input = double( dlmread( '../stereo-3D-calculation/output/points3D.bin', ',' ) );
 
 %% PREPARE DATA
-
-
-%% PLOT SETUP
-
 x = input( :, 1 );
 y = input( :, 2 );
 z = input( :, 3 );
+
+%% PLOT
 
 figure(1)
 plot3( x, y, z, '*' );
@@ -17,6 +15,3 @@ title('3D Point plot')
 xlabel('x-axis') % x-axis label
 ylabel('y-axis') % y-axis label
 zlabel('z-axis') % z-axis label
-
-
-%% PLOT
